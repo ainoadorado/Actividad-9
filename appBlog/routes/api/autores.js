36ed688216fb1651d109971d606e300ca8867be5
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-05-06 19:07:04 +0200

    Creación autores.js

---
+++ appBlog/routes/api/autores.js
@@ -0,0 +1,26 @@
+const { getAll, getById } = require('../../models/autor.model');
+
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const [result] = await getAll();
+        res.json(result);
+    } catch (error) {
+        res.status(500)
+            .json({ fatal: error.message });
+    }
+});
+
+
+
+router.post('/', async (req, res) => {
+    try {
+        const [result] = await create(req.body);
+        const [autor] = await getById(result.insertId);
+        res.json(autor[0]);
+    } catch (error) {
+        console.log(error);
+        res.json({ fatal: error.message });
+    }
+});
